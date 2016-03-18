aeccfb8367fe911056e648f8bcf2e48a935ad7e6
Ivan Ergunov <hozblok@gmail.com>
2016-03-18 15:02:05 -0700

    First visualization!

--- draw/src/parserOwl.py
+++ draw/src/parserOwl.py
@@ -11,16 +11,18 @@ class parserOwl:
         self.path_file = path_file # path to file
         self.flag_getting_element = False # this flag indicates: class collects data to save the element to BD
         self.current_field_name = ""
+        self.list_of_types_of_PhysicalEntity = list(map(lambda x: ":".join((PREFIX_CLASSES, x[0])),
+                                        PhysicalEntity.TYPES_OF_PHYSICALENTITY))
     
     # first handler functions for parser - This is the opening tag
     def start_element(self, name, attrs):
-        list_of_types_of_PhysicalEntity = list(map(lambda x: ":".join((PREFIX_CLASSES, x[0])),
-                                        PhysicalEntity.TYPES_OF_PHYSICALENTITY))
+        
         #+- check for the tag belongs to basic substances
-        if name in list_of_types_of_PhysicalEntity:
+        if name in self.list_of_types_of_PhysicalEntity:
             kind_of = name.split(":", 1)[1]
+            id_in_file = attrs["rdf:ID"].replace(kind_of, "")
             self.element, created = PhysicalEntity.objects.get_or_create(file_owl=self.el_file_owl, id_name=attrs["rdf:ID"],
-                  defaults={'kind_of': kind_of, "display_name" : ""})
+                  defaults={"kind_of": kind_of, "display_name" : "", "id_in_file": id_in_file}) #'id_in_file': id_in_file
             if self.element.display_name != "":
                 self.flag_getting_element = False # we do not change the collected data.
             else:
@@ -48,12 +50,12 @@ class parserOwl:
     # third handler functions for parser - Data between the opening and closing tag
     def char_data(self, data):
         if self.current_field_name != "": # ex. == "display_name", "comment"
-            self.element.__dict__[self.current_field_name] = repr(data)
+            self.element.__dict__[self.current_field_name] = str(data)
             self.current_field_name = ""
     
     # second handler functions for parser - This is the closing tag
     def end_element(self, name):
-        if self.flag_getting_element and name in list_of_types_of_PhysicalEntity:
+        if self.flag_getting_element and name in self.list_of_types_of_PhysicalEntity:
             self.element.save()
             self.flag_getting_element = False
     
